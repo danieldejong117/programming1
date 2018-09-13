b227e9f35028352b36bfc8ab22133e931ef3e60c
danieldejong117 <daniel.dh.dejong@student.nl>
2018-09-13 11:23:40 +0200

    les 2 wijzigen

---
+++ les 4/p4.1.py
@@ -0,0 +1,9 @@
+cijferICOR=6
+cijferPROG=8
+cijferCSN=7
+cijfers = [cijferICOR, cijferPROG, cijferCSN]
+som = sum(cijfers)
+beloning = som * 30
+print ('beloning' + str (beloning))
+overzicht = ('mijn beloning is' + ' ' + str (beloning))
+print (overzicht)
---
+++ les 4/p4.3.py
@@ -0,0 +1,5 @@
+uurloon = ("3.80")
+uren = ("20")
+salaris = eval(uurloon) * eval(uren)
+print (str(uren) + ' ' + "uur werken levert" + ' ' +  str(salaris) + ' ' + "euro op")
+
